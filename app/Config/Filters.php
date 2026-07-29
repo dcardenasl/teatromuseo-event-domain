6a988c5e51050be2b1be21574869a2373457303a
David Cardenas Lorca <cine.dcl@gmail.com>
2026-07-29 11:16:54 -0400

    fix(events): remove pagecache filter to avoid stale public responses

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -12,7 +12,6 @@ use CodeIgniter\Filters\DebugToolbar;
 use CodeIgniter\Filters\ForceHTTPS;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
-use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
 use dcardenasl\Ci4ApiCore\Http\Filters\CorsFilter;
 use dcardenasl\Ci4ApiCore\Http\Filters\FeatureToggleFilter;
@@ -42,7 +41,6 @@ class Filters extends BaseFilters
         'secureheaders'      => SecurityHeadersFilter::class,
         'cors'               => CorsFilter::class,
         'forcehttps'         => ForceHTTPS::class,
-        'pagecache'          => PageCache::class,
         'performance'        => PerformanceMetrics::class,
         'domainauth'         => \App\Filters\DomainAuthFilter::class,
         'webappkey'          => \App\Filters\WebAppKeyRequiredFilter::class,
@@ -62,10 +60,8 @@ class Filters extends BaseFilters
      */
     public array $required = [
         'before' => [
-            'pagecache',
         ],
         'after' => [
-            'pagecache',
             'performance',
             'toolbar',
         ],
