064520ba8d0980d5561f085ae3c12db47b2af1db
David Cardenas Lorca <cine.dcl@gmail.com>
2026-07-31 21:41:17 -0400

    fix(events): rename permission codes to event.* namespace

--- app/Config/Routes/v1/events.php
+++ app/Config/Routes/v1/events.php
@@ -6,87 +6,87 @@ $routes->group('events', ['namespace' => '\App\Controllers\Api\V1\Events'], func
     // Auth & Admin Protected Group
     $routes->group('', ['filter' => ['domainauth', 'throttle']], function ($routes): void {
         // Event Routes
-        $routes->group('', ['filter' => 'permission:events.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.events.read'], function ($routes): void {
             $routes->get('events', 'EventController::index');
             $routes->get('events/(:num)', 'EventController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:events.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.events.write'], function ($routes): void {
             $routes->post('events', 'EventController::create');
             $routes->put('events/(:num)', 'EventController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:events.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.events.delete'], function ($routes): void {
             $routes->delete('events/(:num)', 'EventController::delete/$1');
         });
         // Venue Routes
-        $routes->group('', ['filter' => 'permission:venues.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.venues.read'], function ($routes): void {
             $routes->get('venues', 'VenueController::index');
             $routes->get('venues/(:num)', 'VenueController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:venues.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.venues.write'], function ($routes): void {
             $routes->post('venues', 'VenueController::create');
             $routes->put('venues/(:num)', 'VenueController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:venues.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.venues.delete'], function ($routes): void {
             $routes->delete('venues/(:num)', 'VenueController::delete/$1');
         });
         // Occurrence Routes
-        $routes->group('', ['filter' => 'permission:occurrences.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.occurrences.read'], function ($routes): void {
             $routes->get('occurrences', 'OccurrenceController::index');
             $routes->get('occurrences/(:num)', 'OccurrenceController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:occurrences.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.occurrences.write'], function ($routes): void {
             $routes->post('occurrences', 'OccurrenceController::create');
             $routes->put('occurrences/(:num)', 'OccurrenceController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:occurrences.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.occurrences.delete'], function ($routes): void {
             $routes->delete('occurrences/(:num)', 'OccurrenceController::delete/$1');
         });
         // Event Reference Routes
-        $routes->group('', ['filter' => 'permission:event-references.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.event-references.read'], function ($routes): void {
             $routes->get('event-references', 'EventReferenceController::index');
             $routes->get('event-references/(:num)', 'EventReferenceController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:event-references.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.event-references.write'], function ($routes): void {
             $routes->post('event-references', 'EventReferenceController::create');
             $routes->put('event-references/(:num)', 'EventReferenceController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:event-references.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.event-references.delete'], function ($routes): void {
             $routes->delete('event-references/(:num)', 'EventReferenceController::delete/$1');
         });
         // TicketType Routes
-        $routes->group('', ['filter' => 'permission:ticket-types.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.ticket-types.read'], function ($routes): void {
             $routes->get('ticket-types', 'TicketTypeController::index');
             $routes->get('ticket-types/(:num)', 'TicketTypeController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:ticket-types.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.ticket-types.write'], function ($routes): void {
             $routes->post('ticket-types', 'TicketTypeController::create');
             $routes->put('ticket-types/(:num)', 'TicketTypeController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:ticket-types.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.ticket-types.delete'], function ($routes): void {
             $routes->delete('ticket-types/(:num)', 'TicketTypeController::delete/$1');
         });
         // Booking Routes
-        $routes->group('', ['filter' => 'permission:bookings.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.bookings.read'], function ($routes): void {
             $routes->get('bookings', 'BookingController::index');
             $routes->get('bookings/(:num)', 'BookingController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:bookings.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.bookings.write'], function ($routes): void {
             $routes->post('bookings', 'BookingController::create');
             $routes->put('bookings/(:num)', 'BookingController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:bookings.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.bookings.delete'], function ($routes): void {
             $routes->delete('bookings/(:num)', 'BookingController::delete/$1');
         });
         // Ticket Routes
-        $routes->group('', ['filter' => 'permission:tickets.read'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.tickets.read'], function ($routes): void {
             $routes->get('tickets', 'TicketController::index');
             $routes->get('tickets/(:num)', 'TicketController::show/$1');
         });
-        $routes->group('', ['filter' => 'permission:tickets.write'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.tickets.write'], function ($routes): void {
             $routes->post('tickets', 'TicketController::create');
             $routes->put('tickets/(:num)', 'TicketController::update/$1');
         });
-        $routes->group('', ['filter' => 'permission:tickets.delete'], function ($routes): void {
+        $routes->group('', ['filter' => 'permission:event.tickets.delete'], function ($routes): void {
             $routes->delete('tickets/(:num)', 'TicketController::delete/$1');
         });
     });
