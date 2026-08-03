c4627ce6ddcd5bd2a5a3dff68754c7fc8dc79bfa
David Cardenas Lorca <cine.dcl@gmail.com>
2026-08-03 15:18:03 -0400

    chore(hub): version file metadata cache keys

--- app/Libraries/Hub/HubClient.php
+++ app/Libraries/Hub/HubClient.php
@@ -116,6 +116,6 @@ class HubClient extends CoreHubClient
 
     private function fileMetaCacheKey(int $fileId): string
     {
-        return 'hub_file_meta_' . $fileId;
+        return 'hub_file_meta_v2_' . $fileId;
     }
 }
